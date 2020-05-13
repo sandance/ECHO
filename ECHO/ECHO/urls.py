187bea47aaef33c7bfaffc94458a8ed2d33acc1f
Ringo <naimul.islam1@northsouth.edu>
2020-05-14 01:52:54 +0600

    Django Setup

--- ECHO/ECHO/urls.py
+++ ECHO/ECHO/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from home.views import HomepageView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomepageView.as_view(), name='home'),
 ]
