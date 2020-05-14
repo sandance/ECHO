5767caa8bdfd184e5c9896d41a4a86bf25b69e56
Ringo <naimul.islam1@northsouth.edu>
2020-05-14 21:04:43 +0600

    Template loaded to Django project

--- ECHO/ECHO/settings.py
+++ ECHO/ECHO/settings.py
@@ -117,4 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    '/ECHO/ECHO/static'
+]
+
 STATIC_URL = '/static/'
